a3f09b25fb99a30cf1f8c5a955af243304109e4f
Sanskriti Lamichhane <lamichhanesans@gmail.com>
2024-03-08 23:31:46 +0545

    Inside cafeadminsidebar.blade.php added a code that uses active nav link according to specified routes.

--- resources/views/layouts/cafeadmincomponent/cafeadminsidebar.blade.php
+++ resources/views/layouts/cafeadmincomponent/cafeadminsidebar.blade.php
@@ -1,13 +1,13 @@
 <nav class="text-white text-base font-semibold pt-3">
-    <a href="index.html" class="flex items-center active-nav-link text-white py-4 pl-6 nav-item">
+    <a href="index.html" class="flex items-center  {{Request::is('cafedashboard')?'active':''}} text-white py-4 pl-6 nav-item">
         <i class="fas fa-tachometer-alt mr-3"></i>
         Dashboard
     </a>
-    <a href="{{url('cafecategories')}}" class="flex items-center text-white opacity-75 hover:opacity-100 py-4 pl-6 nav-item">
+    <a href="{{url('cafecategories')}}" class="flex items-center {{Request::is('cafecategories')?'active':''}} text-white opacity-75 hover:opacity-100 py-4 pl-6 nav-item">
         <i class="fas fa-sticky-note mr-3"></i>
         Categories
     </a>
-    <a href="{{url('add-cafecategory')}}" class="flex items-center text-white opacity-75 hover:opacity-100 py-4 pl-6 nav-item">
+    <a href="{{url('add-cafecategory')}}" class="flex items-center {{Request::is('add-cafecategory')?'active':''}} text-white opacity-75 hover:opacity-100 py-4 pl-6 nav-item">
         <i class="fas fa-table mr-3"></i>
         Add Category
     </a>
